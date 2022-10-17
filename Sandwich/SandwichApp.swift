0ffb6f614e9348ec5c50e2b0441e5433696096a6
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2022-10-17 12:18:27 +0700

    Initial Commit

---
+++ Sandwich/SandwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichApp.swift
+//  Sandwich
+//
+//  Created by Matthias Kreier on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SandwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
